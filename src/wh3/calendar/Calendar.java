abeedaa369380597947f51f8c4213c6d2b97cdc8
Wh31028 <wh31028@naver.com>
2024-10-06 22:17:43 +0900

    반복문 break,continue

--- src/wh3/calendar/Calendar.java
+++ src/wh3/calendar/Calendar.java
@@ -21,24 +21,27 @@ public class Calendar {
 	}
 
 	public static void main(String[] args) {
+		String PROMPT = "cal>";
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		
-		System.out.println("반복횟수를 입력하세요.");
-		int n =scanner.nextInt();
-		
-		int[] monthex = new int[n];
-		System.out.println("월을 입력하세요.");
-		for(int i=0;i<n;i++) {
-			monthex[i] = scanner.nextInt();			
-		}
-		for(int i =0;i<n;i++) {
-			System.out.println(monthex[i] + "월은 " + cal.getMaxDaysofMonth(monthex[i]) + "일까지 있습니다.");
+
+		int month = 0;
+
+		while (true) {
+			System.out.println("월을 입력하세요.");
+			System.out.print(PROMPT);
+			month = scanner.nextInt();
+			if (month == -1)
+				break;
+			if (month > 12)
+				continue;
+			System.out.println(month + "월은 " + cal.getMaxDaysofMonth(month) + "일까지 있습니다.");
 		}
 
-		
+		System.out.println("Have a nice day!");
+
 //		cal.printSampleCalendar();
-		
+
 		scanner.close();
 
 	}
