4d93576ecb00004690b788005f89c954358b5c84
Wh31028 <wh31028@naver.com>
2024-10-06 21:28:30 +0900

    calendar println 구

--- src/wh3/calendar/Calendar.java
+++ src/wh3/calendar/Calendar.java
@@ -3,7 +3,13 @@ package wh3.calendar;
 public class Calendar {
 
 	public static void main(String[] args) {
-		 System.out.println("Hello,Calendar");
+		System.out.println("  일 월  화  수 목  금 토");
+		System.out.println("---------------------");
+		for (int i = 1; i < 29; i++) {
+			System.out.print((i < 10) ? "  " + i : " " + i);
+			if (i % 7 == 0)
+				System.out.println();
+		}
 
 	}
 
