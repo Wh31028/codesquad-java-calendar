890c7ddae2424a9c23b7fbce9fd119acc05d406b
Wh31028 <wh31028@naver.com>
2024-10-06 22:54:55 +0900

    캘린더 년 월 입력에 따라 출력하기. + 윤년계산

--- src/wh3/calendar/Calendar.java
+++ src/wh3/calendar/Calendar.java
@@ -1,49 +1,36 @@
 package wh3.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	private final int[] LEAP_MAX_DAYS = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-	public int getMaxDaysofMonth(int month) {
-		return MAX_DAYS[month - 1];
+	public boolean isLeapYear(int year) {
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))
+			return true;
+		else
+			return false;
+	}
+
+	public int getMaxDaysofMonth(int year, int month) {
+		if (isLeapYear(year))
+			return LEAP_MAX_DAYS[month - 1];
+		else
+			return MAX_DAYS[month - 1];
 
 	}
 
-	public void printSampleCalendar() {
-		System.out.println("  일 월  화  수 목  금 토");
+	public void printCalendar(int year, int month) {
+
+		System.out.printf("     <<%4d년%3d월>>\n", year, month);
+		System.out.println(" SU MO TU WE TH FR SA");
 		System.out.println("---------------------");
-		for (int i = 1; i < 29; i++) {
-			System.out.print((i < 10) ? "  " + i : " " + i);
+		for (int i = 1; i <= getMaxDaysofMonth(year,month); i++) {
+			System.out.printf("%3d", i);
+//			System.out.print((i < 10) ? "  " + i : " " + i);
 			if (i % 7 == 0)
 				System.out.println();
 		}
-	}
-
-	public static void main(String[] args) {
-		String PROMPT = "cal>";
-		Scanner scanner = new Scanner(System.in);
-		Calendar cal = new Calendar();
-
-		int month = 0;
-
-		while (true) {
-			System.out.println("월을 입력하세요.");
-			System.out.print(PROMPT);
-			month = scanner.nextInt();
-			if (month == -1)
-				break;
-			if (month > 12)
-				continue;
-			System.out.println(month + "월은 " + cal.getMaxDaysofMonth(month) + "일까지 있습니다.");
-		}
-
-		System.out.println("Have a nice day!");
-
-//		cal.printSampleCalendar();
-
-		scanner.close();
-
+		System.out.println();
 	}
 
 }
