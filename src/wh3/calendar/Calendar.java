b76f415a6521712425eb3695b1e7216de936eff9
Wh31028 <wh31028@naver.com>
2024-10-06 22:09:35 +0900

    Add repeat function

--- src/wh3/calendar/Calendar.java
+++ src/wh3/calendar/Calendar.java
@@ -21,13 +21,23 @@ public class Calendar {
 	}
 
 	public static void main(String[] args) {
-
 		Scanner scanner = new Scanner(System.in);
 		Calendar cal = new Calendar();
-		System.out.println("달을 입력하세요.");
-		int month = scanner.nextInt();
-		System.out.println(month + "월은 " + cal.getMaxDaysofMonth(month) + "일까지 있습니다.");
-		cal.printSampleCalendar();
+		
+		System.out.println("반복횟수를 입력하세요.");
+		int n =scanner.nextInt();
+		
+		int[] monthex = new int[n];
+		System.out.println("월을 입력하세요.");
+		for(int i=0;i<n;i++) {
+			monthex[i] = scanner.nextInt();			
+		}
+		for(int i =0;i<n;i++) {
+			System.out.println(monthex[i] + "월은 " + cal.getMaxDaysofMonth(monthex[i]) + "일까지 있습니다.");
+		}
+
+		
+//		cal.printSampleCalendar();
 		
 		scanner.close();
 
