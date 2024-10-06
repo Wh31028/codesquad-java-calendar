e697edf683564b28fddec25e89587fcb6818aa44
Wh31028 <wh31028@naver.com>
2024-10-06 21:54:02 +0900

    update using class method

--- src/wh3/calendar/Calendar.java
+++ src/wh3/calendar/Calendar.java
@@ -1,8 +1,16 @@
 package wh3.calendar;
-import java.util.Scanner;
-public class Calendar {
 
-	public static void main(String[] args) {
+import java.util.Scanner;
+
+public class Calendar {
+	private final int[] MAX_DAYS = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+	public int getMaxDaysofMonth(int month) {
+		return MAX_DAYS[month - 1];
+
+	}
+
+	public void printSampleCalendar() {
 		System.out.println("  일 월  화  수 목  금 토");
 		System.out.println("---------------------");
 		for (int i = 1; i < 29; i++) {
@@ -10,17 +18,19 @@ public class Calendar {
 			if (i % 7 == 0)
 				System.out.println();
 		}
-		
-		int monthDays[] = {0,31,28,31,30,31,30,31,31,30,31,30,31};
+	}
+
+	public static void main(String[] args) {
+
 		Scanner scanner = new Scanner(System.in);
+		Calendar cal = new Calendar();
 		System.out.println("달을 입력하세요.");
-		int month =scanner.nextInt();
-		System.out.println(month+"월은 "+monthDays[month]+"일까지 있습니다.");
+		int month = scanner.nextInt();
+		System.out.println(month + "월은 " + cal.getMaxDaysofMonth(month) + "일까지 있습니다.");
+		cal.printSampleCalendar();
 		
 		scanner.close();
-		
-		
-		
+
 	}
 
 }
